b0c32a238e7ab01f84c6d10c97c969024fc172c1
Bhavya Goyal <bhavyagoyal777@gmail.com>
2025-02-26 14:36:47 -0600

    minor

--- tools/extrappc/delete_model.sh
+++ tools/extrappc/delete_model.sh
@@ -5,5 +5,17 @@ done
 
 for (( i = 11; i < 40; i++ ))
 do
-	find ../../work_dir_py/kitti/pvrcnn -iname epoch_$i.pth | xargs rm
+	find ../../work_dir_py/kitti/pvrcnn/ -iname epoch_$i.pth | xargs rm
 done
+
+for (( i = 1; i < 12; i++ ))
+do
+	find ../../work_dir_py/sbr/ -iname epoch_$i.pth | xargs rm
+done
+
+for (( i = 13; i < 36; i++ ))
+do
+	find ../../work_dir_py/sbr/ -iname epoch_$i.pth | xargs rm
+done
+
+
